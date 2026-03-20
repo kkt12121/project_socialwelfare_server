6e6de52b54a43df5d44b02421a0f5d3daf3b5f60
kkt12121 <kkt34343@gmail.com>
2026-03-21 00:42:07 +0900

    [2026-03-20]-배포-port-수정

--- config/mysql.config.js
+++ config/mysql.config.js
@@ -1,16 +1,10 @@
 const mysql = require("mysql2/promise"); // Promise 버전 사용
 require("dotenv").config();
 
-console.log(
-  "Connecting to:",
-  process.env.DB_HOST,
-  "Port:",
-  process.env.DB_PORT,
-);
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
