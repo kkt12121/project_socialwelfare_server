5b92e624d88da6921f40db684187d84537090fe6
kkt12121 <kkt34343@gmail.com>
2026-03-20 23:11:35 +0900

    [2026-03-20]-배포-작업

--- config/mysql.config.js
+++ config/mysql.config.js
@@ -4,11 +4,15 @@ require("dotenv").config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
